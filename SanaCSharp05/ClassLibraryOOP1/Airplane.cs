21f5b43d7fc3d019655af442817b36a1981e0647
Дмитро Демченко <demchenkodmytro73@gmail.com>
2023-01-30 22:52:17 +0200

    Airplane&Date

---
+++ SanaCSharp05/ClassLibraryOOP1/Airplane.cs
@@ -0,0 +1,80 @@
+﻿namespace ClassLibraryOOP1
+{
+    public class Airplane
+    {
+        protected string? StartCity;
+        protected string? FinishCity;
+        protected Date StartDate;
+        protected Date FinishDate;
+
+        public Airplane() 
+        { 
+            this.StartDate = new Date();
+            this.FinishDate = new Date();
+            this.startCity = null;
+            this.finishCity = null;
+        }
+
+        public Airplane(string startCity, string finsihCity, Date startDate, Date finishDate)
+        { 
+            this.StartDate = startDate;
+            this.FinishDate = finishDate;
+            this.StartCity = startCity;
+            this.FinishCity = finsihCity;
+        }
+        public Airplane(string startCity, string finsihCity, int startYear, int startMonth,
+            int startDay, int finishYear, int finishMonth, int finishDay)
+        {
+            this.StartCity = startCity;
+            this.FinishCity = finsihCity;
+            this.StartDate = new Date(startYear, startMonth, startDay);
+            this.FinishDate = new Date(finishYear, finishMonth, finishDay);
+        }
+
+        public Airplane(Airplane previousAirplane)
+        { 
+            this.StartDate = previousAirplane.StartDate;
+            this.FinishDate = previousAirplane.FinishDate;
+            this.FinishCity = previousAirplane.FinishCity;
+            this.StartDate = previousAirplane.StartDate;
+        }
+
+        public double GetTotalTime()
+        {
+            TimeSpan result = FinishDate.GetFullDate() - StartDate.GetFullDate();
+            return result.TotalMinutes;
+        }
+
+        public bool IsArrivingToday()
+        { 
+            if(StartDate.day == FinishDate.day && StartDate.month == FinishDate.month && StartDate.year == FinishDate.year)
+                return true;
+            else
+                return false;
+        }
+
+        public string? finishCity
+        {
+            set { FinishCity = value; }
+            get { return FinishCity; }
+        }
+
+        public string? startCity
+        {
+            set { StartCity = value; }
+            get { return StartCity; }
+        }
+
+        public Date finishDate
+        { 
+            set { FinishDate = value; }
+            get { return FinishDate; }
+        }
+
+        public Date startDate
+        {
+            set { StartDate = value; }
+            get { return StartDate; }
+        }
+    }
+}
---
+++ SanaCSharp05/ClassLibraryOOP1/Date.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibraryOOP1
+{
+    public class Date
+    {
+        protected int Year;
+        protected int Month;
+        protected int Day;
+        protected int Hours;
+        protected int Minutes;
+        public Date()
+        {
+           this.Year = DateTime.Now.Year;
+           this.Month = DateTime.Now.Month;
+           this.Day = DateTime.Now.Day;
+           this.Hours = DateTime.Now.Hour;
+           this.Minutes = DateTime.Now.Minute;
+        }
+
+        public Date(int year, int month, int day, int hours, int minutes)
+        {
+            this.Year = year;
+            this.Month = month;
+            this.Day = day;
+            this.Hours = hours;
+            this.Minutes = minutes;
+        }
+
+        public Date(int year, int month, int day) 
+        {
+            this.Year = year;
+            this.Month = month;
+            this.Day = day;
+            this.Hours = DateTime.Now.Hour;
+            this.Minutes = DateTime.Now.Minute;
+        }
+
+        public Date(Date previousDate)
+        {
+            this.Year = previousDate.Year;
+            this.Month = previousDate.Month;
+            this.Day = previousDate.Day; ;
+            this.Hours = previousDate.Hours; ;
+            this.Minutes = previousDate.Minutes;
+        }
+
+        public int year
+        {
+            set{ Year = value; }
+            get{ return Year; }
+        }
+
+        public int hours
+        {
+            set { Hours = value; }
+            get { return Hours; }
+        }
+
+        public int day
+        {
+            set { Day = value; }
+            get { return Day; }
+        }
+
+        public int minutes
+        {
+            set { Minutes = value; }
+            get { return Minutes; }
+        }
+        public int month
+        {
+            set { Month = value; }
+            get { return Month; }
+        }
+
+        public DateTime GetFullDate()
+        {
+            return new DateTime(this.Year, this.Month, this.Day, this.Hours, this.Minutes, 0);
+        }
+    }
+}
