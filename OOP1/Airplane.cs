228d2b0000d1f1974bd34fe19adf7980f0860c14
Микита Шевцов <ipz222_shms@student.ztu.edu.ua>
2023-01-30 18:41:25 +0200

    Airplane & Date classes

---
+++ OOP1/Airplane.cs
@@ -0,0 +1,77 @@
+﻿namespace OOP1
+{
+    internal class Airplane
+    {
+        private string _startCity = "Unknown";
+        private string _finishCity = "Unknown";
+        private Date _startDate = new();
+        private Date _finishDate = new();
+
+        public string StartCity
+        {
+            get { return _startCity; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    _startCity = value;
+            }
+        }
+
+        public string FinishCity
+        {
+            get { return _finishCity; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    _finishCity = value;
+            }
+        }
+
+        public Date StartDate
+        {
+            get { return _startDate; }
+            set { _startDate = value; }
+        }
+
+        public Date FinishDate
+        {
+            get { return _finishDate; }
+            set { _finishDate = value; }
+        }
+
+        public Airplane() { }
+
+        public Airplane(string startCity, string finishCity, Date startDate, Date finishDate)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+            StartDate = startDate;
+            FinishDate = finishDate;
+        }
+
+        public Airplane(string startCity, string finishCity)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+        }
+
+        public Airplane(Date startDate, Date finishDate)
+        {
+            StartDate = startDate;
+            FinishDate = finishDate;
+        }
+
+        public Airplane(Airplane airplane)
+        {
+            StartCity = airplane.StartCity;
+            FinishCity = airplane.FinishCity;
+            StartDate = airplane.StartDate;
+            FinishDate = airplane.FinishDate;
+        }
+
+        public Airplane Copy()
+        {
+            return new(this);
+        }
+    }
+}
---
+++ OOP1/Date.cs
@@ -0,0 +1,99 @@
+﻿namespace OOP1
+{
+    internal class Date
+    {
+        private int _year = 0;
+        private int _month = 1;
+        private int _day = 1;
+        private int _hours = 0 ;
+        private int _minutes = 0;
+
+        public int Year
+        {
+            get { return _year; }
+            set
+            {
+                if (value >= 0)
+                    _year = value;
+            }
+        }
+
+        public int Month
+        {
+            get { return _month; }
+            set
+            {
+                if (value >= 1 && value <= 12)
+                    _month = value;
+            }
+        }
+
+        public int Day
+        {
+            get { return _day; }
+            set
+            {
+                if (value >= 1 && value <= 31)
+                    _day = value;
+            }
+        }
+
+        public int Hours
+        {
+            get { return _hours; }
+            set
+            {
+                if (value >= 0 && value < 24)
+                    _hours = value;
+            }
+        }
+
+        public int Minutes
+        {
+            get { return _minutes; }
+            set
+            {
+                if (value >= 0 && value < 60)
+                    _minutes = value;
+            }
+        }
+
+        public Date() { }
+
+        public Date(int year, int month, int day, int hours, int minutes)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hours = hours;
+            Minutes = minutes;
+        }
+
+        public Date(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+
+        public Date(int hours, int minutes)
+        {
+            Hours = hours;
+            Minutes = minutes;
+        }
+
+        public Date(Date date)
+        {
+            Year = date.Year;
+            Month = date.Month;
+            Day = date.Day;
+            Hours = date.Hours;
+            Minutes = date.Minutes;
+        }
+
+        public Date Copy()
+        {
+            return new(this);
+        }
+    }
+}
