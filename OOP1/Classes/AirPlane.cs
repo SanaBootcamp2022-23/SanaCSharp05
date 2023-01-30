f12d700ee9320728f2014f8a1145a603c9f041a5
Andrii Volynets <volinec449@gmail.com>
2023-01-31 01:20:48 +0200

    done

---
+++ OOP1/Classes/AirPlane.cs
@@ -0,0 +1,44 @@
+﻿namespace OOP1
+{
+    public class AirPlane
+    {
+        public string StartCity { get; set; } = "";
+        public string FinishCity { get; set; } = "";
+        public Date StartDate { get; set; } = new();
+        public Date FinishDate { get; set; } = new();
+
+        public AirPlane() { }
+
+        public AirPlane(string startCity, string finishCity, Date startDate, Date finishDate) : this(startDate, finishDate)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+        }
+
+        public AirPlane(Date startDate, Date finishDate)
+        {
+            StartDate = startDate;
+            FinishDate = finishDate;
+        }
+
+        public AirPlane(AirPlane clone)
+            : this(clone.StartCity, clone.FinishCity, clone.StartDate, clone.FinishDate)
+        { }
+
+        public double GetTotalTime()
+        {
+            return (FinishDate.ToDateTime() - StartDate.ToDateTime()).TotalMinutes;
+        }
+
+        public bool IsArrivaingToday()
+        {
+            return DateTime.Today <= FinishDate.ToDateTime() && FinishDate.ToDateTime() <= DateTime.Today.AddDays(1);
+        }
+
+        public override string ToString()
+        {
+            return $"{StartCity,-18} {FinishCity,-14} {StartDate,-25} {FinishDate}";
+        }
+    }
+
+}
---
+++ OOP1/Classes/Product.cs
@@ -0,0 +1,32 @@
+﻿namespace OOP1
+{
+    public class Product
+    {
+        public string Name { get; private set; } = "";
+        public double Price { get; private set; }
+        public Currency Cost { get; private set; } = new();
+        public int Quantity { get; private set; }
+        public string Producer { get; private set; } = "";
+        public double Weight { get; private set; }
+
+        public Product() { }
+
+        public Product(string name, double price, Currency cost, int quantity, string producer, double weight)
+        {
+            Name = name;
+            Price = price;
+            Cost = cost;
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
+
+        public Product(Product product)
+            : this(product.Name, product.Price, product.Cost, product.Quantity, product.Producer, product.Weight)
+        { }
+
+        public double GetPriceInUAH() => Price * Cost.ExRate;
+        public double GetTotalPriceInUAH() => Quantity * GetPriceInUAH();
+        public double GetTotalWeight() => Quantity * Weight;
+    }
+}
