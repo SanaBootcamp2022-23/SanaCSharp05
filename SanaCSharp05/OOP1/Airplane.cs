ffc7633fa2da8402aa0cc4c906f4f36010fa9c3d
Vladyslav Geyna <ipz215_gvs@student.ztu.edu.ua>
2023-01-30 22:55:59 +0200

    Implemented task with Aitplane and Date classes

--- SanaCSharp05/OOP1/Airplane.cs
+++ SanaCSharp05/OOP1/Airplane.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,81 @@ namespace OOP1
 {
     public class Airplane
     {
+        private string startCity;
+        private string finishCity;
+        private Date startDate;
+        private Date finishDate;
+        public string StartCity
+        {
+            get { return startCity; }
+            set { if (!string.IsNullOrEmpty(value)) startCity = value; }
+        }
+        public string FinishCity
+        {
+            get { return finishCity; }
+            set { if (!string.IsNullOrEmpty(value)) finishCity = value; }
+        }
+        public Date StartDate
+        {
+            get { return startDate; }
+            set { if (value != null) startDate = new Date(value); }
+        }
+        public Date FinishDate
+        {
+            get { return finishDate; }
+            set { if (value != null) finishDate = new Date(value); }
+        }
+        public Airplane() { }
+        public Airplane(Airplane airplane)
+        {
+            StartCity = airplane.StartCity;
+            FinishCity = airplane.FinishCity;
+            StartDate = new Date(airplane.StartDate);
+            FinishDate = new Date(airplane.FinishDate);
+        }
+        public Airplane(string startCity, string finishCity)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+        }
+        public Airplane(string startCity, string finishCity, Date startDate, Date finishDate) : this(startCity, finishCity)
+        {
+            StartDate = new Date(startDate);
+            FinishDate = new Date(finishDate);
+        }
+        private bool areDatesSetted()
+        {
+            return StartDate != null && FinishDate != null;
+        }
+        public int? GetTotalTime()
+        {
+            if (areDatesSetted())
+            {
+                int totalTravelMinutesTime, yearDiff = 0, monthDiff = 0, dayDiff = 0, hoursDiff = 0, minutesDiff = 0;
+                if (StartDate.Year < FinishDate.Year) yearDiff = (FinishDate.Year - StartDate.Year) * Date.MINUTES_IN_YEAR;
+                if (StartDate.Month < FinishDate.Month) monthDiff = (FinishDate.Month - StartDate.Month) * Date.MINUTES_IN_MONTH;
+                if (StartDate.Day < FinishDate.Day) dayDiff = (FinishDate.Day - StartDate.Day) * Date.MINUTES_IN_DAY;
+                if (StartDate.Hours < FinishDate.Hours) hoursDiff = (FinishDate.Hours - StartDate.Hours) * Date.MINUTES_IN_HOUR;
+                if (StartDate.Minutes < FinishDate.Minutes) minutesDiff = FinishDate.Minutes - StartDate.Minutes;
+                totalTravelMinutesTime = yearDiff + monthDiff + dayDiff + hoursDiff + minutesDiff;
+                return totalTravelMinutesTime;
+            }
+            return null;
+        }
+        public bool? IsArrivingToday()
+        {
+            if (areDatesSetted())
+                return StartDate.Equals(FinishDate);
+            return null;
+        }
+
+
+
+
+
+
+
+
 
     }
 }
--- SanaCSharp05/OOP1/Date.cs
+++ SanaCSharp05/OOP1/Date.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,71 @@ namespace OOP1
 {
     public class Date
     {
+        public const int MINUTES_IN_YEAR = 525600;
+        public const int MINUTES_IN_MONTH = 43800;
+        public const int MINUTES_IN_DAY = 1440;
+        public const int MINUTES_IN_HOUR = 60;
+        private int year;
+        private int month;
+        private int day;
+        private int hours;
+        private int minutes;
+        public int Year
+        {
+            get { return year; }
+            set { if (value > 0) year = value; }
+        }
+        public int Month
+        {
+            get { return month; }
+            set { if (value > 0 && value <= 12) month = value; }
+        }
+        public int Day
+        {
+            get { return day; }
+            set { if (value > 0 && value <= 31) day = value; }
+        }
+        public int Hours
+        {
+            get { return hours; }
+            set { if (value >= 0 && value <= 23) hours = value; }
+        }
+        public int Minutes
+        {
+            get { return minutes; }
+            set { if (value >= 0 && value <= 59) minutes = value; }
+        }
+        public Date() { }
+        public Date(Date date)
+        {
+            Year = date.Year;
+            Month = date.Month;
+            Day = date.Day;
+            Hours = date.Hours;
+            Minutes = date.Minutes;
+        }
+        public Date(int day, int month, int year)
+        {
+            Day = day;
+            Month = month;
+            Year = year;
+        }
+        public Date(int day, int month, int year, int hours, int minutes) : this(day, month, year)
+        {
+            Hours = hours;
+            Minutes = minutes;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Date date)
+            {
+                if (obj == null || !GetType().Equals(obj.GetType()))
+                    return false;
+                else
+                    return (Year == date.Year) && (Month == date.Month) && (Day == date.Day) && (Hours == date.Hours) && (Minutes == date.Minutes);
+            }
+            return false;
+        }
     }
 }
