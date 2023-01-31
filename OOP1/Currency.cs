71cb97bc53e98c51883e072b45fba1dc973a3e50
VitaBrrevis <vitalina.doronina@gmail.com>
2023-01-31 23:50:41 +0200

    i hope this commit works

---
+++ OOP1/Currency.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Data;
+
+public class Currency
+{
+    protected string Name;
+    protected double ExRate;
+    
+    public Currency()
+    {
+        Name = "hrn";
+        ExRate = 1;
+    }
+    public Currency (double exRate)
+    {
+        Name = "idk";
+        ExRate = exRate;
+    }
+    public Currency (string name, double exRate)
+    {
+        Name = name;
+        ExRate = exRate;
+    }
+    public Currency (Currency obj) : this(obj.Name, obj.ExRate) { }
+    public string GetName() => Name;
+    public double GetExRate() => ExRate;
+    public void SetName(string name ) => Name = name;
+    public void SetExRate(double exRate) => ExRate = exRate;
+    public float GetPriceInUAH(float price)
+    {
+        return (float)(price*ExRate); 
+    }
+    public float GetTotalPriceInUAH(float price, int quantity)
+    {
+        return (float)(price * quantity * ExRate);
+    }
+    public float GetTotalWeight(int quantity, float weight)
+    {
+        return (float)(quantity * weight);
+    }
+}
