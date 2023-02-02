2db0e936833299710600384372e42899533f46b1
Dariia Zhurakivska <sana22_zhdv@student.ztu.edu.ua>
2023-02-02 13:03:50 +0200

    Airplane class added

---
+++ SanaCSharp05/OOP1/Date.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Date
+    {
+       public int Year;
+        public int Month;
+        public int Day;
+        public int Hours;
+        public int Minutes;
+        //I named it "public" because I cant't use it in the "Airplane" class when I wanted to get total time
+
+        public Date()
+        {
+            Year = DateTime.Now.Year;
+            Month = DateTime.Now.Month;
+            Day = DateTime.Now.Day;
+            Hours = DateTime.Now.Hour;
+            Minutes = DateTime.Now.Minute;
+        }
+
+        public Date(int year)
+        {
+            Year = year;
+        }
+        public Date(int year, int month)
+        {
+            Year = year;
+            Month = month;
+        }
+        public Date(int year, int month, int day)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+        }
+        public Date(int year, int month, int day, int hours)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hours = hours;
+        }
+        public Date(int year, int month, int day, int hours, int minutes)
+        {
+            this.Year = year;
+            this.Month = month;
+            this.Day = day;
+            this.Hours = hours;
+            this.Minutes = minutes;
+        }
+        public Date(Date obj)
+        {
+            this.Year = obj.Year;
+            this.Month = obj.Month;
+            this.Day = obj.Day;
+            this.Hours = obj.Hours;
+            this.Minutes = obj.Minutes;
+        }
+        public int GetYear()
+        {
+            return Year;
+        }
+        public int GetMonth()
+        {
+            return Month;
+        }
+        public int GetDay()
+        {
+            return Day;
+        }
+        public int GetHours()
+        {
+            return Hours;
+        }
+        public int GetMinutes()
+        {
+            return Minutes;
+        }
+        public void SetYear(int year)
+        {
+            if (year > 0 && year <= 2023)
+            {
+                Year = year;
+            }
+        }
+        public void SetMonth(int month)
+        {
+            if (month > 0 && month <= 12)
+            {
+                Month = month;
+            }
+        }
+        public void SetDay(int day)
+        {
+            if (day > 0 && day <= 31)
+            {
+                Day = day;
+            }
+        }
+        public void SetHour(int hour)
+        {
+            if (hour > 0 && hour <= 24)
+            {
+                Hours = hour;
+            }
+        }
+        public void SetMinutes(int minutes)
+        {
+            if (minutes > 0 && minutes <= 60)
+            {
+                Minutes = minutes;
+            }
+        }
+        public DateTime TotalTime()
+        {
+            return new DateTime(this.Year, this.Month, this.Day, this.Hours, this.Minutes, 0);
+        }
+    }
+    }
+
+       
---
+++ SanaCSharp05/OOP1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection;
+using OOP1;
+
+class Program
+
+    {
+    public static void Main(string[] args)
+    {
+        Date startDate = new Date(2023, 2, 5, 17, 30);
+        Date finishDate = new Date(2023, 2, 6, 12, 30);
+        Airplane airplane = new Airplane("Kyiv", "Tokio", startDate, finishDate);
+
+        Console.WriteLine("Airplane");
+        Console.WriteLine("Total time: " + airplane.GetTotalTime());
+        Console.WriteLine("Is arriving today: " + airplane.IsArrivingToday());
+
+        Console.WriteLine("\n");
+
+        Currency currency = new Currency("USD", 800);
+        Product product = new Product("AppleWatch", 800, currency, 40, "Apple", 500);
+
+        Console.WriteLine("Product & Currency");
+        Console.WriteLine("Price in UAH: " + product.GetPriceInUAH());
+        Console.WriteLine("Total price in UAH: " + product.GetTotalPriceInUAH());
+        Console.WriteLine("Total weight: " + product.GetTotalWeight());
+
+        Console.ReadLine();
+    }
+}
