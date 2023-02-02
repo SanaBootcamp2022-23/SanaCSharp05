422c605d4cb869086bb31d68ee1b5fd18adf965b
Volkov <volkov.aliksandr0203@gmail.com>
2023-02-03 00:18:30 +0200

    Created project, added .gitignore file, created some own classes like (Currency, Date, Product, Airplane) and all methods, constructors, properties that demanded in task

---
+++ OOP1/Currency.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Currency
+    {
+        protected string Name;
+        protected double ExRate;
+        
+        public Currency()
+        {
+            this.Name = "гривня";
+            this.ExRate = 1;
+        }
+
+        public Currency(string name, double exRate):this(exRate)
+        {
+            this.Name = name;
+        }
+
+        public Currency(double exRate)
+        { 
+            this.ExRate = exRate;
+            this.Name = "own_currency";
+        }
+
+        public Currency(Currency obj)
+        {
+            this.Name = obj.Name;
+            this.ExRate = obj.ExRate;
+        }
+
+        public string GetNameCurrency()
+        {
+            return this.Name;
+        }
+
+        public double GetExRateCurrency()
+        {
+            return this.ExRate;
+        }
+
+        public void SetNameCurrency(string name)
+        {
+            if (name.Length >= 2)
+            {
+                this.Name = name;
+            }
+        }
+
+        public void SetExRateCurrency(double exRate)
+        {
+            if (exRate > 0)
+            {
+                this.ExRate = exRate;
+            }
+        }
+    }
+}
---
+++ OOP1/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using OOP1;
+Currency currency = new Currency(25);
+Console.WriteLine(currency.GetNameCurrency());
+Console.WriteLine(currency.GetExRateCurrency());
+Console.ReadLine();
