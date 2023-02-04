d49211f3f9e0293e52ca95922f727fd2d3cdea2d
Andrew Vozniuk <ipz224_vav@student.ztu.edu.ua>
2023-02-04 13:54:11 +0200

    All the tasks has been done

---
+++ OOP1/Date.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Date
+    {
+        protected int[] monthNumOfDays = { 31, 28, 31, 30, 31, 20, 31, 31, 30, 31, 30, 31 };
+
+        public int highYear()
+        {
+            int res = 0;
+            if (Year % 4 == 0)
+            {
+                if (Year % 100 == 0)
+                {
+                    if (Year % 400 == 0)
+                    {
+                        res = 1;
+                    }
+                }
+                else
+                {
+                    res = 1;
+                }
+            }
+
+            return res;
+        }
+
+        public int totalDays()
+        {
+            int total = 0;
+
+            for(int i = 1; i < Month; i++)
+            {
+                total += monthNumOfDays[i-1];
+            }
+
+            total += Day;
+
+            return total;
+        }
+
+        public int totalMin()
+        {
+            int total = 0;
+            total = totalDays() * 60 * 24;
+            total += Hour * 60;
+            total += minutes;
+
+            return total;
+        }
+
+        protected int year;
+        public int Year
+        {
+            set
+            {
+                if (value > 2023 && value < 2022)
+                {
+                    year = value;
+                }
+            }
+            get
+            {
+                return year;
+            }
+        }
+
+        protected int month;
+        public int Month
+        {
+            set
+            {
+                if (value < 0 && value > 12)
+                {
+                    month = value;
+                }
+            }
+            get
+            {
+                return month;
+
+            }
+        }
+
+        protected int day;
+        public int Day
+        {
+            set
+            {
+                
+                if (Convert.ToBoolean(highYear()))
+                {
+                    monthNumOfDays[1] += 1;
+                }
+            }
+            get { return day; }
+        }
+
+        protected int hours;
+
+        public int Hour
+        {
+            set
+            {
+                if (value > 0 && value < 60)
+                {
+                    hours = value;
+                }
+            }
+            get { return hours; }
+        }
+        protected int minutes;
+
+        public int Minute
+        {
+            set
+            {
+                if (value > 0 && value < 60)
+                {
+                    minutes = value;
+                }
+            }
+
+            get { return minutes; }
+        }
+
+        public Date()
+        {
+
+        }
+
+        public Date(int year, int month, int day, int hours, int minutes)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        public Date(int year, int month, int day, int hours)
+        {
+            this.year = year;
+            this.month = month;
+            this.day = day;
+            this.hours = hours;
+        }
+
+        public Date(Date date)
+        {
+            year = date.year;
+            month = date.month;
+            day = date.day;
+            hours = date.hours;
+            minutes = date.minutes;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Product
+    {
+        protected string Name;
+        protected int Price;
+        protected Currency Cost;
+        protected int Quantity;
+        protected string Producer;
+        protected int Weight;
+        
+        public Product()
+        {
+
+        }
+
+        public Product(string name, int price, Currency cost, int quantity, string producer, int weight)
+        {
+            Name = name;
+            Price = price;
+            Cost = cost;
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
+
+        public Product(string name, int price, int quantity, string producer)
+        {
+            Name = name;
+            Price = price;
+            Quantity = quantity;
+            Producer = producer;         
+        }
+
+        public Product(Product product)
+        {
+            Name = product.Name;
+            Price = product.Price;
+            Cost = product.Cost;
+            Quantity = product.Quantity;   
+            Producer = product.Producer;
+            Weight = product.Weight;
+        }
+
+        public int GetPriceInUAH() => Cost.exRate * Price;
+        public int GetTotalPriceInUAH() => GetPriceInUAH() * Quantity;
+        public double GetTotalWeight() => Weight * Quantity;
+    }
+}
