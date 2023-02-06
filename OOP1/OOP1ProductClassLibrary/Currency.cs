b0b137b587cb008b8c9c23efa344ac71ae297b92
OleksiyBubenko <ipz202_bov@student.ztu.edu.ua>
2023-02-07 01:21:57 +0200

    Added classes

---
+++ OOP1/OOP1ProductClassLibrary/Currency.cs
@@ -0,0 +1,60 @@
+﻿namespace OOP1ProductClassLibrary;
+
+public class Currency
+{
+    protected double exRate;
+
+    //ЗАХИЩЕНІ ПОЛЯ 
+    protected string name;
+
+    //КОНСТРУКТОР ЗА ЗАМОВЧУВАННЯМ
+    public Currency()
+    {
+        name = "USD";
+        exRate = 36.7;
+    }
+
+    //КОНСТРУКТОРИ З ПАРАМТЕРАМИ
+    public Currency(string name, double exRate)
+    {
+        this.name = name;
+        this.exRate = exRate;
+    }
+
+    public Currency(string name)
+    {
+        this.name = name;
+        exRate = 36.7;
+    }
+
+    public Currency(double exRate)
+    {
+        name = "USD";
+        this.exRate = exRate;
+    }
+
+    //КОНСТРУКТОР КОПІЮВАННЯ
+    public Currency(Currency currency)
+    {
+        name = currency.name;
+        exRate = currency.exRate;
+    }
+
+    //ВЛАСТИВОСТІ для ПОЛІВ
+    public string Name
+    {
+        get => name;
+        set => name = value;
+    }
+
+    public double ExRate
+    {
+        get => exRate;
+        set => exRate = value < 0 ? 0 : value;
+    }
+
+    public string ToString()
+    {
+        return $"{Name} - {ExRate}";
+    }
+}
---
+++ OOP1/OOP1Program/Program.cs
@@ -0,0 +1,29 @@
+﻿using OOP1AirplaneClassLibrary;
+using OOP1ProductClassLibrary;
+
+namespace OOP1Program;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("The OOP1 Program\n");
+
+        // Airplane
+        Console.WriteLine("Airplane:\n");
+
+        var startDate = new Date();
+        var endDate = new Date(16, 24);
+
+        var airplane = new Airplane("Tokio", startDate, endDate);
+
+        Console.WriteLine(airplane.ToString());
+
+        // Product
+        Console.WriteLine("\n\nProduct:\n");
+
+        var product = new Product();
+
+        Console.WriteLine(product.ToString());
+    }
+}
