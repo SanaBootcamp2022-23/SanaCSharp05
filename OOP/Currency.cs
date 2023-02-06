70a5b0bf9e436f64de5f7d77066fb3bc93142400
Oleksandr Hanchevskyi <ipz222_goo@student.ztu.edu.ua>
2023-02-06 21:40:09 +0300

    123

---
+++ OOP/Currency.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    internal class Currency
+    {
+        protected string Name = "NoName";
+        public decimal ExRate = 0;
+        public Currency()
+        {
+            Name = "NoName";
+            ExRate = 0;
+        }
+        public Currency(string name, decimal exRate)
+        {
+            Name = name;    
+            ExRate = exRate;
+        }
+        public Currency(string name)
+        {
+            Name = name;
+        }
+        public Currency(decimal exRate)
+        {
+            ExRate = exRate;
+        }
+        public Currency(Currency obj)
+        {
+            Name=obj.Name;
+            ExRate = obj.ExRate;
+        }
+        public override string ToString()
+        {
+            return $"Ім'я Currency {Name}\nПеревод в грн{ExRate}";
+        }
+        public void SetNameCurrency(string name) { Name = name; }
+        public void SetExRate(decimal exRate) { ExRate = exRate; }
+
+        public string GetNameCurrency() { return Name; }
+        public decimal GetExRate() { return ExRate; }
+    }
+}
+
---
+++ OOP/Date.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP
+{
+    public class Date
+    {
+        protected int Year;
+        protected int Month;
+        protected int Day;
+        protected int Hours;
+        protected int Minutes;
+        public Date()
+        {
+            Year = 2022;
+            Month = 12;
+            Day = 08;
+            Hours = 06;
+            Minutes = 12;
+        }
+        public Date(int year, int month, int day, int hours, int minutes)
+        {
+            Year = year;
+            Month = month;
+            Day = day;
+            Hours = hours;
+            Minutes = minutes;
+        }
+        public Date(int year, int month)
+        {
+            Year = year;
+            Month = month;
+            Day = 08;
+            Hours = 12;
+            Minutes = 08;
+        }
+        public Date(int day, int hours, int minutes)
+        {
+            Year = 2000;
+            Month = 05;
+            Day = day;
+            Hours = hours;  
+            Minutes = minutes;
+
+        }
+        public Date(Date obj)
+        {
+            Year=obj.Year;  
+            Month=obj.Month;
+            Day=obj.Day;
+            Hours=obj.Hours;
+            Minutes=obj.Minutes;
+        }
+        public void SetYear(int year) { Year = year; }
+        public void SetMonth(int month) { Month = month; }  
+        public void SetDay(int day) { Day = day; }
+        public void SetHours(int hours) { Hours = hours; }   
+        public void SetMinutes(int minutes) { Minutes = minutes; }   
+
+        public int GetYear() { return Year; }
+        public int GetMonth() { return Month; }
+        public int GetDay() { return Day; }
+        public int GetHours() { return Hours; }
+        public int GetMinutes() { return Minutes; }
+        
+        public override string ToString()
+        {
+            return $"{Day}.{Month}.{Year} {Hours}:{Minutes}";
+        }
+    }
+}
