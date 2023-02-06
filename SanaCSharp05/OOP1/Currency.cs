d34886893e98c7fd955e06eb50e16dbdad322d73
Andrii Gras <sana22_gav@student.ztu.edu.ua>
2023-02-06 14:42:37 +0200

    project complete

---
+++ SanaCSharp05/OOP1/Currency.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Currency
+    {
+        protected string name;
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        protected double exrate;
+        public double ExRate
+        {
+            get { return exrate; }
+            set
+            {
+                exrate = value;
+            }
+        }
+        public Currency(string name, double exrate, double exRate)
+        {
+            this.name = name;
+
+            this.exrate = exrate;
+            ExRate = exRate;
+        }
+
+        public double GetPriceInUAH(double cost, double exrate)
+        {
+            return exrate * cost;
+        }
+        public double GetTotalPrice(double quantity, double cost)
+        {
+            return (exrate * cost) * quantity;
+        }
+        public double GetTotalWeight(double weight, double quantity)
+        {
+            return weight * quantity;
+        }
+        public Currency(Currency obj)
+        {
+            name = obj.Name;
+            exrate = obj.ExRate;
+        }
+    }
+}
