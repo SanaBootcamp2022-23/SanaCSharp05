48375d5be8ac64c138f1511d74fd3e0e19801593
Yuriy Deyneka <ipz213_dyum@student.ztu.edu.ua>
2023-02-06 15:57:21 +0200

    Added Currency and Product classes with constructors

---
+++ OOP1/Currency.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Currency
+    {
+        private string Name;
+        private float ExRate;
+        public Currency()
+        {
+            Name = "UAH";
+            ExRate = 1;
+        }
+        public Currency(string name, float exRate)
+        {
+            Name = name;
+            ExRate = exRate;
+        }
+        public Currency(string name)
+        {
+            Name = name;
+            ExRate = 1;
+        }
+        public Currency(float exRate)
+        {
+            Name = "";
+            ExRate = exRate;
+        }
+        public Currency(Currency currency)
+        {
+            Name = currency.Name;
+            ExRate = currency.ExRate;
+        }
+    }
+}
---
+++ OOP1/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Product
+    {
+        private string Name;
+        private float Price;
+        private Currency Cost;
+        private int Quantity;
+        private string Producer;
+        private float Weight;
+        
+        public Product()
+        {
+            Name = "No name";
+            Price = 0;
+            Cost = new Currency();
+            Quantity = 0;
+            Producer = "undefined";
+            Weight = 0;
+        }
+        public Product(string name, float price, Currency cost, int quantity, string producer, float weight)
+        {
+            Name = name;
+            Price = price;
+            Cost = cost;
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
+        public Product(string name, float price, int quantity, string producer, float weight)
+        {
+            Name = name;
+            Price = price;
+            Cost = new Currency();
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
+        public Product(Product product)
+        {
+            Name = product.Name;
+            Price = product.Price;
+            Cost = new Currency(product.Cost);
+            Quantity = product.Quantity;
+            Producer = product.Producer;
+            Weight = product.Weight;
+        }
+
+    }
+}
