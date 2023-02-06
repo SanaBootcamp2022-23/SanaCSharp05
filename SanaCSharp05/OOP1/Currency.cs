38414f98bff43d7eb8306f8a9addeb640f0fb38f
Денис Бордюг <ipz224_bdv@student.ztu.edu.ua>
2023-02-06 14:32:13 +0200

    All task Don

---
+++ SanaCSharp05/OOP1/Currency.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static OOP1.Display;
+
+namespace OOP1
+{
+    public class Currency
+    {
+        protected string _name = "Euro";
+        public string Name {
+            set {
+                if (value != null && value != "") 
+                _name = value; 
+            }
+            get { return _name; } 
+        }
+
+        protected double _exRate = 40.21;
+        public double ExRate {
+            set { 
+                if (value>=0.001)
+                    _exRate = value;
+            }
+            get { return _exRate;} 
+        }
+
+        public Currency()
+        {
+
+        }
+        public Currency(Currency currency)
+        {
+            _name = currency.Name;
+            _exRate = currency.ExRate;
+        }
+        public Currency(string name, double exRate)
+        {
+            Name = name;
+            ExRate = exRate;
+        }
+
+        public Currency(string name)
+        {
+            Name = name;
+        }
+    }
+}
---
+++ SanaCSharp05/OOP1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more 
+using OOP1;
+using static OOP1.Display;
+
+Airplane airplane1 = new Airplane();
+airplane1.FinishDate.Hour = 13;
+DisplayAirplane(airplane1);
+Console.WriteLine("Airplane is arriving today - " + airplane1.IsArrivingToday());
+Console.WriteLine("Total Time In Minutes - " + airplane1.GetTotalTime() + " min");
+
+Console.WriteLine("***************");
+
+Airplane airplane2 = new Airplane("Praha", "London");
+airplane2.FinishDate.Day = 9;
+airplane2.FinishDate.Minutes = 0;
+DisplayAirplane(airplane2);
+Console.WriteLine("Airplane is arriving today - " + airplane2.IsArrivingToday());
+Console.WriteLine("Total Time In Minutes - " + airplane2.GetTotalTime() + " min");
+
+Console.WriteLine("-----------------------");
+
+Product product = new Product();
+DisplayProduct(product);
+Console.WriteLine("***************");
+Console.WriteLine("Price in UAH : " + product.GetPriceInUAH() + " UAH");
+Console.WriteLine("Total Price in UAH : " + product.GetTotalPriceInUAH() + " UAH");
+Console.WriteLine("Total weight - " + product.GetTotalWeight() + " kg");
