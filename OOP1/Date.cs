2b0144b2e5e911ac9c78d7de1fd62821e3f5989b
Yuriy Deyneka <ipz213_dyum@student.ztu.edu.ua>
2023-02-06 15:30:54 +0200

    Added Date and Airplane classes with protected fields

---
+++ OOP1/Date.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    public class Date
+    {
+        protected int Day;
+        protected int Month;
+        protected int Year;
+        protected int Hours;
+        protected int Minutes;
+    }
+}
