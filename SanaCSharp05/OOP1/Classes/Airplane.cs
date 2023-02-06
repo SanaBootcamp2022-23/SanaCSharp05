0e86e0090d3abd9dfbbdac3e41b3af1e021c9983
Danyil Tuhaienko <kpz21_tdo@student.ztu.edu.ua>
2023-02-06 10:19:24 +0200

    Done

---
+++ SanaCSharp05/OOP1/Classes/Airplane.cs
@@ -0,0 +1,110 @@
+﻿namespace OOP1.Classes
+{
+    public class Airplane
+    {
+        protected string startCity;
+        protected string finishCity;
+        protected Date startDate;
+        protected Date finishDate;
+
+
+        public string StartCity
+        {
+            get { return startCity; }
+            set
+            {
+                startCity = value ?? string.Empty;
+            }
+        }
+
+        public string FinishCity
+        {
+            get { return finishCity; }
+            set
+            {
+                finishCity = value ?? string.Empty;
+            }
+        }
+        public Date StartDate
+        {
+            get { return startDate; }
+            set
+            {
+                startDate = value ?? new Date();
+            }
+        }
+        public Date FinishDate
+        {
+            get { return finishDate; }
+            set
+            {
+                finishDate = value ?? new Date();
+            }
+        }
+
+        public Airplane()
+        {
+            StartCity = "Kyiv";
+            FinishCity = "Lviv";
+        }
+        public Airplane(string startCity, string finishCity, Date startDate, Date finishDate)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+            StartDate = startDate;
+            FinishDate = finishDate;
+        }
+        public Airplane(string startCity, string finishCity)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+        }
+        public Airplane(Date startDate, Date finishDate)
+        {
+            StartDate = startDate;
+            FinishDate = finishDate;
+        }
+        public Airplane(Date startDate, string startCity)
+        {
+            StartDate = startDate;
+            StartCity = startCity;
+        }
+        public Airplane(Airplane airplane)
+        {
+            StartCity = airplane.StartCity;
+            FinishCity = airplane.FinishCity;
+            StartDate = airplane.StartDate;
+            FinishDate = airplane.FinishDate;
+        }
+
+
+
+        public double GetTotalTime()
+        {
+            var _startDate = new DateTime(StartDate.Year, StartDate.Month, StartDate.Day,
+                StartDate.Hours, StartDate.Minutes, 0);
+            var _finishtDate = new DateTime(FinishDate.Year, FinishDate.Month, FinishDate.Day,
+                            FinishDate.Hours, FinishDate.Minutes, 0);
+
+            var diff = _finishtDate.Subtract(_startDate);
+
+
+
+
+            return diff.TotalMinutes;
+        }
+
+        public bool IsArrivingToday()
+        {
+            if (StartDate.Day == FinishDate.Day && StartDate.Month == FinishDate.Month
+                && StartDate.Year == FinishDate.Year)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ SanaCSharp05/OOP1/Classes/Currency.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1.Classes
+{
+    public class Currency
+    {
+        private string name;
+        private float exRate;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public float ExRate
+        {
+            get { return exRate; }
+            set { exRate = value; }
+        }
+        public Currency(string name, float exRate)
+        {
+            Name = name;
+            ExRate = exRate;
+        }
+        public Currency(Currency currency)
+        {
+            Name = currency.Name;
+            ExRate = currency.ExRate;
+        }
+        public Currency(string name)
+        {
+            Name = name;
+            ExRate = 0;
+        }
+        public Currency(float exRate)
+        {
+            Name = string.Empty;
+            ExRate = exRate;
+        }
+        public Currency()
+        {
+            Name = string.Empty;
+            ExRate = 0;
+        }
+    }
+}
