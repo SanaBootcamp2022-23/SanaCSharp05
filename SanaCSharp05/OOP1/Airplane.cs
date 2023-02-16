d8a42a49a14c292cfae362567c065f66caf75de6
Anton Osypchuk <osypchuk.ua@gmail.com>
2023-02-16 19:39:41 +0200

    Lab 5 done

---
+++ SanaCSharp05/OOP1/Airplane.cs
@@ -0,0 +1,50 @@
+﻿namespace OOP1
+{
+    public class Airplane
+    {
+        protected string StartCity { get; set; }
+        protected string FinishCity { get; set; }
+        protected Date? StartDate { get; set; }
+        protected Date? FinishDate { get; set; }
+
+        public Airplane()
+        {
+            StartCity = "";
+            FinishCity = "";
+            StartDate = null;
+            FinishDate = null;
+        }
+
+        public Airplane(string startCity, string finishCity, Date startDate, Date finishDate)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+            StartDate = startDate;
+            FinishDate = finishDate;
+        }
+
+        public Airplane(string startCity, string finishCity)
+        {
+            StartCity = startCity;
+            FinishCity = finishCity;
+        }
+
+        public Airplane(Airplane airplane)
+        {
+            StartCity = airplane.StartCity;
+            FinishCity = airplane.FinishCity;
+            StartDate = airplane.StartDate;
+            FinishDate = airplane.FinishDate;
+        }
+
+        public int? GetTotalTime()
+        {
+            return FinishDate.GetTotalMinutes() - StartDate.GetTotalMinutes();
+        }
+
+        public bool IsArrivingToday()
+        {
+            return GetTotalTime() > (24 * 60);
+        }
+    }
+}
---
+++ SanaCSharp05/OOP1/Product.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP1
+{
+    internal class Product
+    {
+        protected string Name { get; set; }
+        protected double Price { get; set; }
+        protected Currency Cost { get; set; }
+        protected int Quantity { get; set; }
+        protected string Producer { get; set; }
+        protected int Weight { get; set; }
+        
+        public Product() 
+        { 
+            Name = string.Empty;
+            Price = 0;
+            Cost = new Currency();
+            Quantity = 0;
+            Producer = string.Empty;
+            Weight = 0;
+        }
+
+        public Product(string name, double price, Currency cost, int quantity, string producer, int weight)
+        {
+            Name = name;
+            Price = price;
+            Cost = cost;
+            Quantity = quantity;
+            Producer = producer;
+            Weight = weight;
+        }
+
+        public Product(string name, double price, Currency cost, int quantity, string producer)
+        {
+            Name = name;
+            Price = price;
+            Cost = cost;
+            Quantity = quantity;
+            Producer = producer;
+            Weight = 0;
+        }
+
+        public Product(Product product)
+        {
+            Name = product.Name;
+            Price = product.Price;
+            Cost = product.Cost;
+            Quantity = product.Quantity;
+            Producer = product.Producer;
+            Weight = product.Weight;
+        }
+
+        public double GetPriceInUAH()
+        {
+            if (Cost.Name == "UAH")
+            {
+                return Price;
+            }
+            return Cost.ExRate * Price;
+        }
+
+        public double GetTotalPriceInUAH()
+        {
+            return GetPriceInUAH() * Quantity;
+        }
+
+        public int GetTotalWeight()
+        {
+            return Weight * Quantity;
+        }
+    }
+}
